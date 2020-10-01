39c2e41c6b14e7bbd30d6902cfcf4a05a1451a0e
Pedro Knup <p.knup@cowmanager.com>
2020-10-01 18:06:18 +0200

    Changed font

--- src/main.tsx
+++ src/main.tsx
@@ -5,18 +5,18 @@ import { createBrowserHistory } from 'history';
 import { Router, Switch, Route } from 'react-router';
 // import { HomeComponent } from './components/pages/home';
 import { HomeComponent } from './components/pages/home';
-import { createGlobalStyle } from 'styled-components';
+// import { createGlobalStyle } from 'styled-components';
 
 import operatorRegular from './assets/fonts/OperatorMono/OperatorMonoBookRegular.otf';
 
-createGlobalStyle`
-    @font-face {
-        font-family: Operator Mono;
-        src: url(${operatorRegular}) format('opentype');
-        font-weight: normal;
-        font-style: normal;
-    }
-`;
+// createGlobalStyle`
+//     @font-face {
+//         font-family: Operator Mono;
+//         src: url(${operatorRegular}) format('opentype');
+//         font-weight: normal;
+//         font-style: normal;
+//     }
+// `;
 
 export const history = createBrowserHistory();
 ReactDOM.render(
