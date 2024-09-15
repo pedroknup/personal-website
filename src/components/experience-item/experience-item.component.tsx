931c06717322222d0f82a78b5581fb502af55b39
Pedro Knup <pedro.knup.barros@rabobank.nl>
2024-09-15 11:11:44 +0200

    Experience item uses markdown

--- src/components/experience-item/experience-item.component.tsx
+++ src/components/experience-item/experience-item.component.tsx
@@ -4,17 +4,17 @@ import { ExperienceItemProps } from '../../../types/experience';
 import Markdown from 'markdown-to-jsx';
 
 export const ExperienceItem = ({ defaultOpened, isDark, icon, date, title, place, description }: ExperienceItemProps) => {
-  const [isOpen, setIsOpen] = useState(defaultOpened);
+  const [isExpanded, setIsExpanded] = useState(defaultOpened);
 
   const toggleOpen = () => {
-    setIsOpen(!isOpen);
+    setIsExpanded(!isExpanded);
   };
 
-  return <div onClick={toggleOpen} className={`experience-item ${isOpen ? 'open' : ''} ${isDark ? '' : 'light'}`}>
+  return <div onClick={toggleOpen} className={`experience-item ${isExpanded ? 'open' : ''} ${isDark ? '' : 'light'}`}>
     <div className="main-content">
       <div className="wrapper">
         <div className="toggle">
-          <span className={`chevron2 ${isOpen ? 'bottom' : 'right'}`} />
+          <span className={`chevron2 ${isExpanded ? 'bottom' : 'right'}`} />
         </div>
         <div className="picture">
           <img src={icon} />
@@ -26,17 +26,16 @@ export const ExperienceItem = ({ defaultOpened, isDark, icon, date, title, place
         <div className="name">{`//${'*'} ${place}`}</div>
       </div>
     </div>
-    <div className={`content3 ${isOpen ? 'open' : ''}`}>
-      {description.content.web.split('\n').map((item: string, i: number) => {
-        return <p key={i}>{item}</p>;
-      })}
+    <div className={`content3 ${isExpanded ? 'open' : ''}`}>
+      <Markdown style={{ maxWidth: 'calc(100vw - 200px)', textAlign: 'justify', whiteSpace: 'break-spaces' }}>{description.content.web}</Markdown>
+
       {description.projects &&
         <div>
           <br />
 
           Projects Highlights:
           {description.projects.map((item, index) => {
-            return <p key={index}><span className="project-title">{item.title}</span>: <Markdown>{item.title}</Markdown></p>;
+            return <p key={index}><span className="project-title">{item.title}</span>: <Markdown>{item.content}</Markdown></p>;
           })}
         </div>
       }
