414b741b2280e451085228f718cfae80ceacd190
Pedro Knup <phknup@gmail.com>
2025-05-21 02:28:26 +0200

    Fix imports in bio and about pages

--- src/components/about/about.component.tsx
+++ src/components/about/about.component.tsx
@@ -1,38 +1,42 @@
 import React from 'react';
 import './about.style.scss';
 import { SectionPage } from '../section';
-import personalData from '../../data';
+import { personalData } from '../../data/bio';
 import { PageProps } from '../../../types/page-props';
 
 export const AboutPage = ({ darkMode, removeHighlight, isHighlighted }: PageProps) => {
-  return <div className={`about-container ${darkMode ? '' : 'light'}`}>
-    <SectionPage darkMode={darkMode} removeHighlight={removeHighlight} isHighlighted={isHighlighted} id="about" paddingColumns={2} title="About">
-      <span className="obj">
-        <span className="brackets">
-          {`{`} <br />
+  return (
+    <div className={`about-container ${darkMode ? '' : 'light'}`}>
+      <SectionPage
+        darkMode={darkMode}
+        removeHighlight={removeHighlight}
+        isHighlighted={isHighlighted}
+        id="about"
+        paddingColumns={2}
+        title="About"
+      >
+        <span className="obj">
+          <span className="brackets">
+            {`{`} <br />
+          </span>
+          &nbsp;&nbsp;&nbsp;&nbsp;<span className="property">name</span>:{' '}
+          <span className="string">"{personalData.name}"</span>, <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;<span className="property">age</span>:{' '}
+          <span className="number">{personalData.age}</span>, <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;<span className="property">nationality</span>:{' '}
+          <span className="string">"{personalData.nationality}"</span>, <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;<span className="property">based</span>:{' '}
+          <span className="string">"{personalData.based}",</span>
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;<span className="property">bio</span>:{' '}
+          <span className="string bio">`{personalData.bio}`</span>
+          <br />
+          <span className="brackets">
+            {`}`} <br />
+          </span>
         </span>
-        &nbsp;&nbsp;&nbsp;&nbsp;<span className="property">name</span>: <span className="string">
-          "{personalData.name}"
-        </span>, <br />
-        &nbsp;&nbsp;&nbsp;&nbsp;<span className="property">
-          age
-        </span>: <span className="number">{personalData.age}</span>, <br />
-        &nbsp;&nbsp;&nbsp;&nbsp;<span className="property">
-          nationality
-        </span>: <span className="string">"{personalData.nationality}"</span>, <br />
-        &nbsp;&nbsp;&nbsp;&nbsp;<span className="property">
-          based
-        </span>: <span className="string">"{personalData.based}",</span>
         <br />
-        &nbsp;&nbsp;&nbsp;&nbsp;<span className="property">
-          bio
-        </span>: <span className="string bio">`{personalData.bio}`</span>
-        <br />
-        <span className="brackets">
-          {`}`} <br />
-        </span>
-      </span>
-      <br />
-    </SectionPage>
-  </div>;
+      </SectionPage>
+    </div>
+  );
 };
--- src/components/intro/intro.component.tsx
+++ src/components/intro/intro.component.tsx
@@ -3,7 +3,7 @@ import Typist from 'react-typist';
 import { SocialIcon } from 'react-social-icons';
 import './intro.style.scss';
 import '../../styles/main.scss';
-import personalData from '../../data';
+import { personalData } from '../../data/bio';
 
 export type IntroPageProps = {
   onFinish: () => void;
@@ -12,53 +12,87 @@ export type IntroPageProps = {
   currentStep: number;
   currentState: number;
   darkMode: boolean;
-}
+};
 
-export const IntroPageComponent = ({ onFinish, hasScrolled, darkMode, previousState, currentState }: IntroPageProps) => {
+export const IntroPageComponent = ({
+  onFinish,
+  hasScrolled,
+  darkMode,
+  previousState,
+  currentState,
+}: IntroPageProps) => {
   const [hasFinishedTyping, setHasFinishedTyping] = useState(false);
 
-  useEffect(
-    () => {
-      if (hasFinishedTyping) {
-        onFinish();
-      }
-    },
-    [hasFinishedTyping]
-  );
+  useEffect(() => {
+    if (hasFinishedTyping) {
+      onFinish();
+    }
+  }, [hasFinishedTyping]);
 
-  return <div className={`intro-container ${darkMode ? '' : 'light'}`}>
-    <div className="profile-pic">
-      <img src={personalData.profilePic} />
-    </div>
-    <div className={`intro-text ${hasFinishedTyping ? 'finished' : ''}`}>
-      {hasFinishedTyping && <span>/*</span>}
-        <span  style={{ marginLeft: hasFinishedTyping ? 15 : 0, display: 'block' }}>
-          {currentState === 0 && previousState == undefined && <Typist onTypingDone={() => {
+  return (
+    <div className={`intro-container ${darkMode ? '' : 'light'}`}>
+      <div className="profile-pic">
+        <img src={personalData.profilePic} />
+      </div>
+      <div className={`intro-text ${hasFinishedTyping ? 'finished' : ''}`}>
+        {hasFinishedTyping && <span>/*</span>}
+        <span style={{ marginLeft: hasFinishedTyping ? 15 : 0, display: 'block' }}>
+          {currentState === 0 && previousState == undefined && (
+            <Typist
+              onTypingDone={() => {
                 setHasFinishedTyping(true);
-              }} avgTypingDelay={hasScrolled ? 0 : 40} cursor={{ hideWhenDone: false, element: '' }} startDelay={currentState === 0 ? 1000 : 0}>
-              {currentState === 0 && <span>
+              }}
+              avgTypingDelay={hasScrolled ? 0 : 40}
+              cursor={{ hideWhenDone: false, element: '' }}
+              startDelay={currentState === 0 ? 1000 : 0}
+            >
+              {currentState === 0 && (
+                <span>
                   Hi! I’m Pedro Knup
                   <br />
                   I love coding and creating creative content :)
                   <br />
-                </span>}
+                </span>
+              )}
               <span />
-            </Typist>}
+            </Typist>
+          )}
         </span>
         {hasFinishedTyping && <span>/*</span>}
       </div>
 
       <div className="contact">
         <div className="social-icons">
-          <SocialIcon  bgColor="transparent"  target='_blank' url="https://www.linkedin.com/in/pedroknup" />
-          <SocialIcon  bgColor="transparent" target='_blank' url="https://www.facebook.com/pedroknup" />
-          <SocialIcon  bgColor="transparent" target='_blank' url="https://www.github.com/pedroknup" />
-          <SocialIcon  bgColor="transparent" target='_blank' url="https://www.instagram.com/pedroknup" />
-          <SocialIcon  bgColor="transparent" target='_blank' url="https://www.pedroknup.deviantart.com" />
+          <SocialIcon
+            bgColor="transparent"
+            target="_blank"
+            url="https://www.linkedin.com/in/pedroknup"
+          />
+          <SocialIcon
+            bgColor="transparent"
+            target="_blank"
+            url="https://www.facebook.com/pedroknup"
+          />
+          <SocialIcon
+            bgColor="transparent"
+            target="_blank"
+            url="https://www.github.com/pedroknup"
+          />
+          <SocialIcon
+            bgColor="transparent"
+            target="_blank"
+            url="https://www.instagram.com/pedroknup"
+          />
+          <SocialIcon
+            bgColor="transparent"
+            target="_blank"
+            url="https://www.pedroknup.deviantart.com"
+          />
         </div>
         <div className="signature">
           made with <span style={{ color: 'red' }}>♥</span> by <span>Pedro Knup </span>
         </div>
       </div>
-    </div>;
+    </div>
+  );
 };
