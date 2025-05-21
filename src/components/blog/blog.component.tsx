6dd95f32af71672c3d796906254b5f70b0d70f35
Pedro Knup <phknup@gmail.com>
2025-05-21 02:46:54 +0200

    Improve copy

--- src/components/blog/blog.component.tsx
+++ src/components/blog/blog.component.tsx
@@ -5,18 +5,32 @@ import './blog.style.scss';
 import { PageProps } from '../../../types/page-props';
 
 export const BlogPage = ({ isDarkMode: darkMode, removeHighlight, isHighlighted }: PageProps) => {
-  return <div className="blog-container">
-    <SectionPage paddingColumns={2} isDarkMode={darkMode} removeHighlight={removeHighlight} isHighlighted={isHighlighted} id="blog" title="Artworks" description="My latest illustrations and videos">
-      {youtubeVideos.map((video, key) => <div key={`yt-${key}`}>
-        <div className="date">{video.date}</div>
-        <a href={video.link} target="_blank" className="title">{video.title}</a>
-        <br />
-        <div className="name">{`//${'*'} ${video.description} ${'*'}//`}</div>
-        <br />
-        <div dangerouslySetInnerHTML={{ __html: video.content }} />
-        <br />
-        <br />
-      </div>)}
-    </SectionPage>
-  </div>;
+  return (
+    <div className="blog-container">
+      <SectionPage
+        paddingColumns={2}
+        isDarkMode={darkMode}
+        removeHighlight={removeHighlight}
+        isHighlighted={isHighlighted}
+        id="blog"
+        title="Artworks"
+        description="My latest artworks and passion projects"
+      >
+        {youtubeVideos.map((video, key) => (
+          <div key={`yt-${key}`}>
+            <div className="date">{video.date}</div>
+            <a href={video.link} target="_blank" className="title">
+              {video.title}
+              <br />
+              <div className="name">{`//${'*'} ${video.description} ${'*'}//`}</div>
+              <br />
+              <div dangerouslySetInnerHTML={{ __html: video.content }} />
+            </a>
+            <br />
+            <br />
+          </div>
+        ))}
+      </SectionPage>
+    </div>
+  );
 };
